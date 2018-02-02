7dce790d665934920fb313e14b2f14fc0a363955
Andrew Duthie <andrew@andrewduthie.com>
2018-02-02 12:03:56 -0500

    Tests: Add integration tests for history, confirm prompt

--- test/e2e/integration/001-hello-gutenberg.js
+++ test/e2e/integration/001-hello-gutenberg.js
@@ -8,4 +8,21 @@ describe( 'Hello Gutenberg', () => {
 		cy.url().should( 'include', 'post-new.php' );
 		cy.get( '[placeholder="Add title"]' ).should( 'exist' );
 	} );
+
+	it( 'Should have no history', () => {
+		cy.get( '.editor-history__undo:not( :disabled )' ).should( 'not.exist' );
+		cy.get( '.editor-history__redo:not( :disabled )' ).should( 'not.exist' );
+	} );
+
+	it( 'Should not prompt to confirm unsaved changes', ( done ) => {
+		cy.window().then( ( window ) => {
+			window.addEventListener( 'beforeunload', ( event ) => {
+				expect( event.returnValue ).to.be.empty;
+
+				done();
+			} );
+		} );
+
+		cy.reload();
+	} );
 } );
